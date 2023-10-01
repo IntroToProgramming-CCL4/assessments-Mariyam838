1b076966df108c777160a0df1b452e5a4672911d
Mariyam838 <144931174+Mariyam838@users.noreply.github.com>
2023-10-01 13:10:59 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 3 date and time.py.py
@@ -0,0 +1,4 @@
+import datetime
+current_date_time=datetime.datetime.now()
+formatted_date_time=current_date_time.strftime("%Y-%m-%d %H:%M:%S")
+print("current date and time:",formatted_date_time)
---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4 String concatination.py.py
@@ -0,0 +1,5 @@
+string1= "Hello!"
+string2=  "Welcome to Metaversage Institute."
+string3= "Learn and enjoy here."
+output_string=string1 + string2 + string3
+print(output_string)
