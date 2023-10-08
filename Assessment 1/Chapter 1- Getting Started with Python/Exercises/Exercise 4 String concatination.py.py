9479c879e1c649c2556480d9854b80c52baa0b3e
Mariyam838 <144931174+Mariyam838@users.noreply.github.com>
2023-10-08 13:07:47 +0400

    Add files via upload

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4 String concatination.py.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 4 String concatination.py.py
@@ -1,5 +1,10 @@
+# Assigning a string to the vairable 'string1'.
 string1= "Hello!"
+#Assigning a string to the variable  'string2'.
 string2=  "Welcome to Metaversage Institute."
+#Assigning a string to the variable 'string3'.
 string3= "Learn and enjoy here."
+#Concatenating strings 'string1', 'string2', and 'string3' into 'output_string'.
 output_string=string1 + string2 + string3
+#Printing the concatenated string.
 print(output_string)
