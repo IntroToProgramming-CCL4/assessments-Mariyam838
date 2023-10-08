7c61a413af894fbb7d50b3cbad6e1175348c05d7
Mariyam838 <144931174+Mariyam838@users.noreply.github.com>
2023-10-08 13:03:56 +0400

    Add files via upload

--- Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 1 print string specific format.py.py
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Exercise 1 print string specific format.py.py
@@ -1,3 +1,5 @@
+#Printing the lyrics of" Twinkle, twinkle, Little Star"
+#Using triple-quotes for a muulti-lne string to represent the lyrics.
 print("""Twinkle, twinkle, little star 
         How I wonder what you are
                 Up above the world so high
