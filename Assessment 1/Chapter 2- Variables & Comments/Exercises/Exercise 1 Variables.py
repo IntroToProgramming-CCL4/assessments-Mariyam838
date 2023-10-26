4d6d646ac770f1a1163b3823b4e63711117b54ad
Mariyam838 <144931174+Mariyam838@users.noreply.github.com>
2023-10-26 20:03:16 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise 1 Variables.py
@@ -0,0 +1,6 @@
+# Assigning the string "Hello, World!" to the variable message
+message= "Hello, World!"
+# Reassigning the string "Goodbye, World!" to the variable message (overwriting the previous value)
+message= "Goodbye,World!"
+# Printing the value of the variable message
+print (message)
