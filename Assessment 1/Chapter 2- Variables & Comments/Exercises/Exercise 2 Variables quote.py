418a9ac110965f29fe4db745a20d7b7cd3c0da87
Mariyam838 <144931174+Mariyam838@users.noreply.github.com>
2023-10-26 22:35:21 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 2- Variables & Comments/Exercises/Exercise 2 Variables quote.py
@@ -0,0 +1,5 @@
+#Define the quote and its author
+quote=("“A friend is one of the best things you can be and the greatest things you can have.” ")
+author= "Sarah Valdez"
+#Print the formatted quote and its author
+print(f"{author} once said,{quote}")
